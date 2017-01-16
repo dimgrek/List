606eaf07a319e61b0054737cd130224860e96d4c
John Nash <dimgrek@gmail.com>
2017-01-16 09:38:14 +0200

    Core setup

---
+++ List/App.cs
@@ -0,0 +1,14 @@
+﻿using List.ViewModels;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+
+namespace List
+{
+    public class App : MvxApplication
+    {
+        public App()
+        {
+            Mvx.RegisterSingleton(new MvxAppStart<CardsListViewModel>());
+        }
+    }
+}
