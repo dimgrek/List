a6325c236ef18a852958fc5dcd733a2bd6f186e8
John Nash <dimgrek@gmail.com>
2017-01-17 09:50:25 +0200

    AppCompat added

--- Droid/Views/CardsList.cs
+++ Droid/Views/CardsList.cs
@@ -1,10 +1,11 @@
 using Android.App;
 using Android.OS;
+using Android.Widget;
 using MvvmCross.Droid.Views;
 
 namespace List.Droid.Views
 {
-    [Activity(Label = "List", MainLauncher = true)]
+    [Activity(Label = "List")]
     public class CardsList : MvxActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,6 +13,11 @@ namespace List.Droid.Views
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.CardsList);
 
+            //var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //Toolbar will now take on default Action Bar characteristics
+            //SetSu(toolbar);
+            //You can now use and reference the ActionBar
+            ActionBar.Title = "Hello from Toolbar";
         }
 
         //public new CardsListViewModel ViewModel
