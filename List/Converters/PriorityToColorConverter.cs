c27fb057c11ee07007c835dab6c3dcd863dbfb55
John Nash <dimgrek@gmail.com>
2017-01-17 17:19:42 +0200

    Readonly colors introduced

--- List/Converters/PriorityToColorConverter.cs
+++ List/Converters/PriorityToColorConverter.cs
@@ -6,20 +6,24 @@ namespace List.Converters
 {
     public class PriorityToColorValueConverter : MvxColorValueConverter
     {
+        private readonly MvxColor LowColor = new MvxColor(121, 196, 69);
+        private readonly MvxColor MediumColor = new MvxColor(247, 148, 22);
+        private readonly MvxColor TopColor = new MvxColor(255, 0, 0);
+        private readonly MvxColor WhiteColor = new MvxColor(255, 255, 255);
+
         protected override MvxColor Convert(object value, object parameter, CultureInfo culture)
         {
             var priority = (Priority) value;
             switch (priority)
             {
                 case Priority.Low:
-                    return new MvxColor(121, 196, 69);
+                    return LowColor;
                 case Priority.Medium:
-                    return new MvxColor(247, 148, 22);
+                    return MediumColor;
                 case Priority.Top:
-                    return new MvxColor(255, 0, 0);
+                    return TopColor;
                 default:
-                    return new MvxColor(255, 255, 255);
-                    ;
+                    return WhiteColor;
             }
         }
     }
