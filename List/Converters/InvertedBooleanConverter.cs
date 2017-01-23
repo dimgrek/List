d119ce1c9a3fabd426e79049093bb137ef01437d
John Nash <dimgrek@gmail.com>
2017-01-23 19:14:28 +0200

    InvertedBooleanConverter added

---
+++ List/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MvvmCross.Platform.Converters;
+
+namespace List
+{
+	public class InvertedBooleanConverter : MvxValueConverter<bool, bool>
+	{
+		protected override bool Convert(bool value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return !value;
+		}
+
+	}
+}
